19f75e12a3bf080410b3bce2ead2787a00da6365
itojun-1230 <itojum1230@gmail.com>
2026-02-16 20:28:00 +0900

    fix: 環境変数の読み込みを修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { fileURLToPath } from 'node:url';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import icon from "astro-icon";
+import dotenv from 'dotenv';
 
 const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
+dotenv.config();
 
 export default defineConfig({
 	site: process.env.SITE_URL,
