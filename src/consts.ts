76e39da7a825aa34da52f7dc2e40246a0d45a240
itojun-1230 <itojum1230@gmail.com>
2026-02-17 00:01:58 +0900

    feat: 追加のリンクセクションを作成し、外部サービスへのリンクを表示

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,30 @@ export const SITE_DESCRIPTION = 'itojum.dev';
 
 export const X_URL = 'https://x.com/itojum1230';
 export const GITHUB_URL = 'https://github.com/itojum';
+
+export const LINKS = [
+  {
+    service: 'GitHub',
+    url: GITHUB_URL,
+    icon: 'ri:github-fill',
+    name: 'itojum',
+  },
+  {
+    service: 'X',
+    url: X_URL,
+    icon: 'ri:twitter-x-line',
+    name: '@itojum1230',
+  },
+  {
+    service: 'VRChat',
+    url: "https://vrchat.com/home/user/usr_e7576e86-ee7d-4083-9561-9cba619067d1",
+    icon: null,
+    name: 'いとじゅん',
+  },
+  {
+    service: 'speakerdeck',
+    url: "https://speakerdeck.com/itojum",
+    icon: null,
+    name: 'itojum',
+  },
+]
